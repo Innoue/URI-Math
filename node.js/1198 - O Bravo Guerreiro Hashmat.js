21114c45a5c6d69bfbd5dfa597d1ddff30d8f407
Jalapa <53586272+Jalapa@users.noreply.github.com>
2019-11-21 15:43:11 -0300

    O Bravo Guerreiro Hashmat - Exercise

---
+++ node.js/1198 - O Bravo Guerreiro Hashmat.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('./node.js/1198.txt', 'utf8')
+let lines = input.split('\n')
+
+for (let item of lines) {
+    item = item.split(" ")
+    let ally = item[0]
+    let enemy = item[1]
+
+    console.log(enemy - ally)
+}
