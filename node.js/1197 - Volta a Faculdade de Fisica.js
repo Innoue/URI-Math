a9a00babae7e4026a81f50d50a9c383d2acf2dbf
Jalapa <53586272+Jalapa@users.noreply.github.com>
2019-11-21 15:42:50 -0300

    Voltar a Faculdade de Física - Exercise

---
+++ node.js/1197 - Volta a Faculdade de Fisica.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('./node.js/1197.txt', 'utf8')
+let lines = input.split('\n')
+
+for (let item of lines) {
+    item = item.split(" ")
+    let v = item[0]
+    let t = item[1]
+
+    console.log(v * (t * 2))
+}
