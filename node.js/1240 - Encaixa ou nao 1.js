a32238b9a395a8ffb7c85d69e923f09da4ecfb32
Jalapa <rodrigoinoue1122@gmail.com>
2019-12-08 23:29:59 -0300

    Create 1240 - Encaixa ou nao 1.js

---
+++ node.js/1240 - Encaixa ou nao 1.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('./node.js/1170.txt', 'utf8')
+let lines = input.split('\n')
+let numberInputs = lines.shift()
+let inputs = lines
+
+for (i of inputs) {
+    let itens = i.split(" ")
+    if (itens[0])
+}
