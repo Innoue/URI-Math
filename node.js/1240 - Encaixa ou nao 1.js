05765a63f8323eb0e855d2ac74d0e89df03eb14a
Jalapa <rodrigoinoue1122@gmail.com>
2020-02-18 22:13:15 -0300

    change

--- node.js/1240 - Encaixa ou nao 1.js
+++ node.js/1240 - Encaixa ou nao 1.js
@@ -1,9 +1,15 @@
-let input = require('fs').readFileSync('./node.js/1170.txt', 'utf8')
+let input = require('fs').readFileSync('./node.js/1240.txt', 'utf8')
 let lines = input.split('\n')
 let numberInputs = lines.shift()
 let inputs = lines
 
 for (i of inputs) {
     let itens = i.split(" ")
-    if (itens[0])
+    console.log(itens)
+    itens[1].replace("\r", "")
+    if (itens[0].includes(itens[1])) {
+        console.log("encaixa")
+    } else {
+        console.log("nao encaixa")
+    }
 }
